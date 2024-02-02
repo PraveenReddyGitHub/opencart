7d57bd51fa2db94f8009c820475470a1b8476eeb
PraveenReddyGitHub <praveenreddy3112@gmail.com>
2024-02-02 14:15:40 +0530

    modifying the firstname

--- opencart/src/test/java/pageObjects/AccountRegistrationPage.java
+++ opencart/src/test/java/pageObjects/AccountRegistrationPage.java
@@ -17,7 +17,7 @@ WebDriver driver;
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(id="input-firstname")
+	@FindBy(id="input-firs")
 	WebElement firstName;
 	
 	@FindBy(id="input-lastname")
